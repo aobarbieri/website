a5cbd9dea69f82e567cc2a5a24a86390c382d79e
aobarbieri <aobarbieri@gmail.com>
2022-02-21 17:45:02 -0500

    Total redesign of website. Change of  Layouts and Styles. Index and Projects page finished. Resume  is currently being worked on

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@ module.exports = {
 	content: ['./dist/**/*.html'],
 	theme: {
 		extend: {
-			fontFamily: {
-				worksans: ['"Work Sans"', 'sans-serif'],
-			},
 			height: {
 				112: '28rem',
 				128: '32rem',
