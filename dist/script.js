fcf2b37f099bbbbe6f7816828e7cced041353a7d
aobarbieri <aobarbieri@gmail.com>
2022-02-24 15:59:58 -0500

    hash links and styles updates

--- dist/script.js
+++ dist/script.js
@@ -6,3 +6,25 @@ $(document).ready(() => {
 		$(this).find('i').toggleClass('far fa-window-close');
 	});
 });
+
+// move slowly to the target
+const workLink = document.querySelector('.workLink');
+const contactLink = document.querySelector('.contactLink');
+const navWorkLink = document.querySelector('.navWorkLink');
+const navContactLink = document.querySelector('.navContactLink');
+
+workLink.addEventListener('click', smoothScroll);
+contactLink.addEventListener('click', smoothScroll);
+navWorkLink.addEventListener('click', smoothScroll);
+navContactLink.addEventListener('click', smoothScroll);
+
+function smoothScroll(event) {
+	event.preventDefault();
+	const href = this.getAttribute('href');
+	const offsetTop = document.querySelector(href).offsetTop;
+
+	scroll({
+		top: offsetTop,
+		behavior: 'smooth',
+	});
+}
