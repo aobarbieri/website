5c92fd03448f0a160bd87eaed22f7731bd43d7b2
aobarbieri <aobarbieri@gmail.com>
2022-02-25 12:33:40 -0500

    fix: replace svg icons with fontAwesome icons

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
 	// MENU
 	$('.navbar__menu-btn').on('click', function () {
 		$('.navbar__links').toggleClass('active');
-		$(this).find('i').toggleClass('fas fa-bars');
+		$(this).find('i').toggleClass('fa-solid fa-bars');
 		$(this).find('i').toggleClass('far fa-window-close');
 	});
 });
